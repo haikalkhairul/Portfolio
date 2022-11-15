534c441baec61eb7447d0c474b0c20bca279129c
Haikal Khairul <haikalaqimi@gmail.com>
2022-11-15 21:48:08 +0000

    Add intro

--- dev-portfolio/src/components/Intro.jsx
+++ dev-portfolio/src/components/Intro.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className='text-4x1 md:text-7x1 dark:text-white mb-1 md:mb-3 font-bold'>
+                Haikal Khairul
+            </h1>
+            <p className='text-base md:text-x1 mb-3 font-medium'>
+                Software Engineer & Web Developer{' '}
+            </p>
+            <p className='text-sm max-w-x1 mb-6 font-bold'>
+                Hi there. Welcome.
+            </p>
+        </div>
+    )
 }
 
-export default Intro
+export default Intro;
